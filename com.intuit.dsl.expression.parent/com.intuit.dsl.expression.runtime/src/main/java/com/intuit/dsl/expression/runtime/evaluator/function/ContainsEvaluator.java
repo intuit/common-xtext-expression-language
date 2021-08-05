779f03c81ca9158ae035e93f719e9e20a8117927
Aureus, Carlo <carlo_aureus@intuit.com>
2021-08-04 17:26:50 -0700

    cleanup, use stream instead of for

--- com.intuit.dsl.expression.parent/com.intuit.dsl.expression.runtime/src/main/java/com/intuit/dsl/expression/runtime/evaluator/function/ContainsEvaluator.java
+++ com.intuit.dsl.expression.parent/com.intuit.dsl.expression.runtime/src/main/java/com/intuit/dsl/expression/runtime/evaluator/function/ContainsEvaluator.java
@@ -10,6 +10,7 @@ import com.intuit.dsl.expression.runtime.model.NumberValue;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Objects;
+import java.util.stream.StreamSupport;
 
 @Slf4j
 public class ContainsEvaluator implements Evaluator<Contains> {
@@ -24,12 +25,9 @@ public class ContainsEvaluator implements Evaluator<Contains> {
   @Override
   public DataValue evaluate(final Contains contains) {
     DataValue input = visitor.doSwitch(contains.getInput());
-    if (input.getType() == DataValue.Type.NULL)
-      return new DataValue(false, DataValue.Type.BOOLEAN);
 
     if (input.getType() == DataValue.Type.STRING || input.getType() == DataValue.Type.NUMBER) {
       DataValue search = visitor.doSwitch(contains.getSearch());
-      if (search.getType() == DataValue.Type.NULL) new DataValue(false, DataValue.Type.BOOLEAN);
       if (search.getType() == DataValue.Type.STRING || search.getType() == DataValue.Type.NUMBER) {
         return new DataValue(input.toString().contains(search.toString()), DataValue.Type.BOOLEAN);
       }
@@ -37,37 +35,28 @@ public class ContainsEvaluator implements Evaluator<Contains> {
     if (input.getType() == DataValue.Type.ARRAY) {
       DataValue search = visitor.doSwitch(contains.getSearch());
       ArrayNode arrayNode = ArrayNode.class.cast(input.getValue());
-      if (search.getType() == DataValue.Type.NULL) new DataValue(false, DataValue.Type.BOOLEAN);
-      if (search.getType() == DataValue.Type.STRING) {
-        return new DataValue(containsElementWithSubString(arrayNode, (String)search.getValue()), DataValue.Type.BOOLEAN);
-      }
-      if (search.getType() == DataValue.Type.NUMBER) {
-        return new DataValue(containsNumber(arrayNode, (NumberValue)search.getValue()), DataValue.Type.BOOLEAN);
+      if (search.getType() == DataValue.Type.STRING || search.getType() == DataValue.Type.NUMBER) {
+        return new DataValue(containsElementWithSubString(arrayNode, search.toString()), DataValue.Type.BOOLEAN);
       }
     }
     return new DataValue(false, DataValue.Type.BOOLEAN);
   }
 
-  private Object containsNumber(ArrayNode arrayNode, NumberValue numberToSearch) {
-    for (JsonNode jsonNode : arrayNode) {
-      if (jsonNode.isNumber() && !Objects.isNull(jsonNode.decimalValue())) {
-        if (jsonNode.decimalValue().equals(numberToSearch.get())) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
   private boolean containsElementWithSubString(ArrayNode arrayNode, String subsString) {
-    for (JsonNode jsonNode : arrayNode) {
-      if (jsonNode.isTextual() && !Objects.isNull(jsonNode.asText())) {
-        if (jsonNode.asText().contains(subsString)) {
-          return true;
+    return StreamSupport
+      .stream(arrayNode.spliterator(), false)
+      .filter(jsonNode -> jsonNode.isNumber() || jsonNode.isTextual() )
+      .map(jsonNode -> {
+        if (jsonNode.isNumber()) {
+          return jsonNode.decimalValue().toString();
         }
-      }
-    }
-    return false;
+        if (jsonNode.isTextual()) {
+          return jsonNode.asText();
+        }
+        return null;
+      })
+      .filter(Objects::nonNull)
+      .anyMatch(string -> string.contains(subsString));
   }
 
 }
